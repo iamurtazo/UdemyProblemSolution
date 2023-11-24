9b0355cd9028cd4e97dab471fa2568ea4dc42db9
Murtazo Toshniyozov <129582119+iamurtazo@users.noreply.github.com>
2023-11-24 19:50:25 +0500

    Add files via upload

---
+++ Problem9/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string input; 
+        do
+        {
+            Console.WriteLine("Enter a few space-separated words: ");
+            input = Console.ReadLine();
+            if(!input.Contains(" "))
+                Console.WriteLine("Please enter valid input!");
+        } while(!input.Contains(" "));
+
+        Console.WriteLine(PascalCaseMaker(input));
+    }
+
+    static string PascalCaseMaker(string input)
+    {
+        string finalOutput = "";
+        var inputSplit = input.Split(" ");
+        for(int i = 0; i < inputSplit.Length; i++)
+        {
+            finalOutput += char.ToUpper(inputSplit[i][0]) + inputSplit[i].Substring(1).ToLower();
+        }
+ 
+        return finalOutput;  
+    }
+}
