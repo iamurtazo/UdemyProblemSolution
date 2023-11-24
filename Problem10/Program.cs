101338769a3039dc5980afc5448defc8c683b5dc
Murtazo Toshniyozov <129582119+iamurtazo@users.noreply.github.com>
2023-11-24 20:33:54 +0500

    Add files via upload

---
+++ Problem10/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+        Console.WriteLine(Vowels(input));
+    }
+    static int Vowels(string input)
+    {
+        int count = 0;
+        foreach(char l in input)
+        {
+            if (l=='a' || l=='e' || l == 'o'|| l=='u' || l == 'i')
+                count++;
+        }
+        return count;
+    }
+
+}
