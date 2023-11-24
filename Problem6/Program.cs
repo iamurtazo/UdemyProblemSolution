f9d4931e18f957d5fee37cf013fa276fd00875e7
Murtazo Toshniyozov <129582119+iamurtazo@users.noreply.github.com>
2023-11-24 11:33:48 +0500

    Add files via upload
    Problem6

---
+++ Problem6/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string input;
+        do
+        {
+            Console.WriteLine("Enter hyphen-separated numbers: ");
+            input = Console.ReadLine();
+            if(!input.Contains("-"))
+                Console.WriteLine("Please enter hyphen-separated numbers!");
+            
+
+        } while(!input.Contains("-"));
+
+        var result = IsConsecutive(input);
+        Console.WriteLine(result);
+
+        
+    }
+    static string IsConsecutive(string input)
+    {
+        string isConsecutive = "Consecutive";
+
+        var inputSplitted = input.Split("-");
+        var intArray = inputSplitted.Select(x => int.Parse(x)).ToArray();
+
+        for(int i = 0; i < intArray.Length - 1; i++)
+        {
+            if(intArray[i] > intArray[i+1])
+            {
+                isConsecutive = "Not consecutive";
+            }
+
+        }
+        return isConsecutive;
+    }
+
+
+}
