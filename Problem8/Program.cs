2a2dc766aca909b9504c0406b01972f1102a8137
Murtazo Toshniyozov <129582119+iamurtazo@users.noreply.github.com>
2023-11-24 18:59:21 +0500

    Add files via upload

---
+++ Problem8/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string? timeValue = Console.ReadLine();
+        
+    }
+    static void IsValidTime(string timeValue)
+    {
+        var timeSplit = timeValue.Split(":");
+
+        foreach(var chalk in timeSplit)
+            Console.Write(chalk + " ");
+        Console.WriteLine();
+
+        var intTimeArr = timeSplit.Select(x => int.Parse(x)).ToArray();
+        if(intTimeArr[0] >= 00 && intTimeArr[0] <= 23 && intTimeArr[1] >= 01 && intTimeArr[1] <= 59)
+            Console.WriteLine("Valid");
+        else Console.WriteLine("Not valid");
+    }
+
+
+
+}
