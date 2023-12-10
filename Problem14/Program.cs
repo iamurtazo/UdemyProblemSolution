e4babe4e77f3dc4a714bb76cca5ec19d41f1aded
iamurtazo <murtazo207@inha.edu>
2023-12-11 02:08:37 +0500

    Problem14

---
+++ Problem14/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var stack = new Stack();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+
+        stack.Clear();
+        Console.WriteLine(stack.Pop());
+    }
+}
+
+public class Stack
+{
+    private readonly ArrayList arraylist = new ArrayList();
+    
+    public void Push(object obj)
+    {
+        if(obj == null)
+            throw new InvalidOperationException("Object can not be null");
+        arraylist.Add(obj);
+        
+    }
+    public object Pop()
+    {
+        if(arraylist.Count == 0)
+            throw new InvalidOperationException("Stack is empty");
+        
+
+        var lastItem = arraylist[arraylist.Count - 1];
+
+        arraylist.RemoveAt(arraylist.Count - 1);
+
+        return lastItem;
+        
+    } 
+    public void Clear()
+    {
+       arraylist.Clear();
+    }
+}
